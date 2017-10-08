c7f3d10a1a14945f1845ff5861c446121efaeb6c
RomainRui <romain.rui10@gmail.com>
2017-10-08 21:52:23 +0200

    add conf files.

--- Sources/KeepinAPI/main.swift
+++ Sources/KeepinAPI/main.swift
@@ -8,6 +8,11 @@ print("hello, world");
 
 let drop = try Droplet()
 
+// will load 0.0.0.0 or 127.0.0.1 based on above config
+let host = drop.config["server", "host"]?.string ?? "0.0.0.0"
+// will load 9000, or environment variable port.
+let port = drop.config["server", "port"]?.int ?? 8080
+
 drop.get("hello") { req in
     return "Hello Vapor"
 }
