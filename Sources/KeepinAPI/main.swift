c5c8a55693df80c8939125e535bda33a77070c9d
RomainRui <romain.rui10@gmail.com>
2017-10-08 20:09:24 +0200

    Import Vapor & install CTLS.

--- Sources/KeepinAPI/main.swift
+++ Sources/KeepinAPI/main.swift
@@ -1,6 +1,15 @@
 // Find a way to add a flag during the compilation.
 // For example: swift build -c debug -Xswiftc -DQA
+import Vapor
 
 #if QA
 print("hello, world");
 #endif
+
+let drop = try Droplet()
+
+drop.get("hello") { req in
+    return "Hello Vapor"
+}
+
+try drop.run()
